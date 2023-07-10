605d50f79eaef2f243634a9cd326c32cdf744e5e
Sara Pineda Lujan <Sariidemg@gmail.com>
2023-07-10 12:18:30 -0500

    Todos los archivos

---
+++ hola-mundo/git.js
@@ -0,0 +1,5 @@
+//usar git init para crear un repositorio 
+//git status para ver los repositorios creados
+// git add para añadir un archivo nuevo al repositorio
+//git commit -m  para guardar
+//git diff para ver diferencias entre ediciones
